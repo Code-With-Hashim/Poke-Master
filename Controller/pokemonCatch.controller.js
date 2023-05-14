b787cf5608b040a2bcb64ce78e42d9af7a7dd373
Mohammad Hassim <107465263+Code-With-Hashim@users.noreply.github.com>
2023-05-14 09:03:29 +0000

    Update 6 files

--- Controller/pokemonCatch.controller.js
+++ Controller/pokemonCatch.controller.js
@@ -1,8 +1,17 @@
-const axios = require('axios')
+const axios = require("axios");
 const ballModifiers = require("../ballModifier.json");
+const { userModel } = require("../model/userDetail");
 const { userInvModal } = require("../model/userInventory");
 const { userPokeModal } = require("../model/userPoke");
-const { userPokemon, wildPokemon, ProperMoveName } = require("./battle.controller");
+const {
+  userPokemon,
+  wildPokemon,
+  ProperMoveName,
+  setInActiveBattle,
+  calculateDamage,
+  setOpponentPokemonDmg,
+  battleBeginFight,
+} = require("./battle.controller");
 
 let reply_markup;
 
@@ -10,49 +19,61 @@ async function MyListPokeballs(bot, query) {
   const chatId = query.message.chat.id;
   const userId = query.from.id;
   const messageId = query.message.message_id;
-   reply_markup = query.message.reply_markup.inline_keyboard
+  reply_markup = query.message.reply_markup.inline_keyboard;
 
-  console.log(query)
+  console.log(query);
 
   try {
-    const { pokeBalls } = await userInvModal.findOne({ owner: userId });
-
-    const inline_pokeBalls = [];
-
-    let trainerBalls = [];
-
-    for (let i=0; i<pokeBalls.length; i++) {
-       trainerBalls.push(isMake(pokeBalls[i].name))
-       
-       if(pokeBalls.length%3 === 0) {
-           if(trainerBalls.length%3 === 0) {
-             inline_pokeBalls.push(trainerBalls)
-         trainerBalls = []
-           }
-       } else if (pokeBalls.length % 2 === 0) {
-             inline_pokeBalls.push(trainerBalls)
-         trainerBalls = []
-       } else {
-             inline_pokeBalls.push(trainerBalls)
-         trainerBalls = []
-       }
-    }
+    const inline_pokeBalls = await makeMyPokeBallList(userId)
     
 
-     inline_pokeBalls.push([{
-       text : "🔙",
-       callback_data : `poke-back` 
-     }])
-
-     bot.editMessageReplyMarkup({inline_keyboard : inline_pokeBalls} , {
-        chat_id : chatId,
-        message_id : messageId
-    })
-
-    
+    bot.editMessageReplyMarkup(
+      { inline_keyboard: inline_pokeBalls },
+      {
+        chat_id: chatId,
+        message_id: messageId,
+      }
+    );
   } catch (err) {
     console.log(err);
   }
+}
+
+
+async function makeMyPokeBallList(userId) {
+  
+      const { pokeBalls } = await userInvModal.findOne({ owner: userId });
+
+  
+  const inline_pokeBalls = [];
+
+    let trainerBalls = [];
+
+    for (let i = 0; i < pokeBalls.length; i++) {
+      trainerBalls.push(isMake(pokeBalls[i].name));
+
+      if (pokeBalls.length % 3 === 0) {
+        if (trainerBalls.length % 3 === 0) {
+          inline_pokeBalls.push(trainerBalls);
+          trainerBalls = [];
+        }
+      } else if (pokeBalls.length % 2 === 0) {
+        inline_pokeBalls.push(trainerBalls);
+        trainerBalls = [];
+      } else {
+        inline_pokeBalls.push(trainerBalls);
+        trainerBalls = [];
+      }
+    }
+
+    inline_pokeBalls.push([
+      {
+        text: "🔙",
+        callback_data: `poke-back`,
+      },
+    ]);
+    
+    return inline_pokeBalls
 }
 
 function isMake(ballName) {
@@ -65,91 +86,214 @@ function isMake(ballName) {
   };
 }
 
-
-async function pokemonCatch(bot , query) {
+async function pokemonCatch(bot, query) {
   // ballName, level, wildLevel, hasCaught, baseSpeed, types
-    // maxHP, currentHP, catchRate, ballModifier, statusModifier
+  // maxHP, currentHP, catchRate, ballModifier, statusModifier
 
-    const chatId = query.message.chat.id
-    const userId = query.from.id
-    const message_id = query.message.message_id
-    const usePokeBall = query.data.trim().split(" ")[1] //ballName
-    
+  const chatId = query.message.chat.id;
+  const userId = query.from.id;
+  const message_id = query.message.message_id;
+  const usePokeBall = query.data.trim().split(" ")[1]; //ballName
+
   try {
-    const userPokemonLevel = userPokemon().level // level
-    const wildPokemonLevel = wildPokemon().level //wildLevel
-    const hasCaught = await userHavePokemon(wildPokemon().name , userId) // hasCaught
-    const wildBaseSpeed = wildPokemon().baseStats.speed // baseSpeed
-    const wildTypes = wildPokemon().type // types
-    const maxHP = wildPokemon().stats.totalhp //maxHP
-    const currentHP = wildPokemon().stats.currenthp
-    const catchRate = await getWildCatchRate(wildPokemon().name)
-    const ballModifier = calculateBallModifier(usePokeBall, userPokemonLevel, wildPokemonLevel, hasCaught, wildBaseSpeed, wildTypes)
-    const statusModifier = 1
-    
-    console.log(maxHP , currentHP , catchRate , ballModifier , statusModifier)
-    
-    
-     const catchChance = calculateCatchRate(maxHP, currentHP, catchRate, ballModifier, statusModifier)
-     
-     const isCaught = isPokemonCaught(catchChance)
-     
-     let message = `You used one ${ProperMoveName(usePokeBall)} \n`
-     
-     
-      
-    
-  } catch(err) {
-    console.log(err)
+    const userPokemonLevel = userPokemon().level; // level
+    const wildPokemonLevel = wildPokemon().level; //wildLevel
+    const hasCaught = await userHavePokemon(wildPokemon().name, userId); // hasCaught
+    const wildBaseSpeed = wildPokemon().baseStats.speed; // baseSpeed
+    const wildTypes = wildPokemon().type; // types
+    const maxHP = wildPokemon().stats.totalhp; //maxHP
+    const currentHP = wildPokemon().stats.currenthp;
+    const catchRate = await getWildCatchRate(wildPokemon().name);
+    const ballModifier = calculateBallModifier(
+      usePokeBall,
+      userPokemonLevel,
+      wildPokemonLevel,
+      hasCaught,
+      wildBaseSpeed,
+      wildTypes
+    );
+    const statusModifier = 1;
+    const randomMove = Math.floor(
+      Math.random() * (wildPokemon().moves.length - 1)
+    );
+
+    const catchChance = calculateCatchRate(
+      maxHP,
+      currentHP,
+      catchRate,
+      ballModifier,
+      statusModifier
+    );
+
+    // makeAuserBallName(usePokeBall)
+    console.log(userId);
+    userInvModal
+      .updateOne(
+        { owner: userId, "pokeBalls.name": makeAuserBallName(usePokeBall) },
+        { $inc: { "pokeBalls.$.stock": -1 } },
+        {
+          new: true,
+        }
+      )
+      .then( async (res) => {
+        const response = await userInvModal.updateOne(
+          { owner: userId, "pokeBalls.stock": 0 },
+          { $pull: { pokeBalls: { stock: 0 } } },
+          { new: true }
+        );
+
+
+        if (res.matchedCount >= 1) {
+          const isCaught = isPokemonCaught(catchChance);
+
+          let message = `You used one ${ProperMoveName(usePokeBall)} \n`;
+
+          let MakeCount = isCaught ? 4 : Math.floor(Math.random() * 4);
+          let count = 0;
+          const delay = calculateThrowTime();
+          const InterValId = setInterval(() => {
+            count++;
+            if (count >= MakeCount && isCaught) {
+              isCaughtSuccess(bot, usePokeBall, chatId, message_id, userId);
+              setInActiveBattle();
+              clearInterval(InterValId);
+            } else if (count >= MakeCount && !isCaught) {
+              isCaughtFailed(wildPokemon, usePokeBall, bot, chatId, message_id , delay , userId , query);
+              //  setInActiveBattle()
+              clearInterval(InterValId);
+            } else {
+              message = message + " " + "•";
+              bot.editMessageText(message, {
+                chat_id: chatId,
+                message_id,
+              });
+            }
+          }, delay);
+        }
+      });
+
+    // console.log(isCaught)
+  } catch (err) {
+    console.log(err);
   }
+}
+
+function makeAuserBallName(useBall) {
+  const ballName = useBall.trim().split("-").join(" ");
+
+  console.log(ballName);
+  return ballName;
 }
 
 function calculateThrowTime() {
-  const randomTimer = Math.floor(Math.random() * 3000 + 1) + 2000
-  
-  return randomTimer
+  const randomTimer = Math.floor(Math.random() * 4000 + 1) + 1000;
+
+  return randomTimer;
 }
 
-
 function isPokemonCaught(calculatedCatchRate) {
-  console.log(calculatedCatchRate)
   const randomValue = Math.random() * 100; // Generate a random number between 0 and 100
+  console.log(randomValue, calculatedCatchRate);
   return randomValue <= calculatedCatchRate; // Compare the random value with the calculated catch rate
 }
 
-
 async function getWildCatchRate(pokeName) {
-  
   try {
-    
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}`)
-    
-    const catchRate = response.data.capture_rate
-    
-    return catchRate
-    
-  } catch(err) {
-    console.log(err)
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokeName}`
+    );
+
+    const catchRate = response.data.capture_rate;
+
+    return catchRate;
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function isCaughtSuccess(bot, usePokeBall, chatId, message_id, userId) {
+  message = `You used one ${ProperMoveName(usePokeBall)} \n ☆ ☆ ☆`;
+  bot.editMessageText(message, {
+    chat_id: chatId,
+    message_id,
+  });
+
+  try {
+    await userPokeModal
+      .create({ ...wildPokemon(), trainer: userId })
+      .then(() => {
+        bot
+          .editMessageText("You Caught Wild Pokemon", {
+            chat_id: chatId,
+            message_id,
+          })
+          .then(() => {
+            bot.editMessageReplyMarkup(
+              {
+                inline_keyboard: [
+                  [
+                    { text: "View Poke Stats", callback_data: "viewPokeStats" },
+                    { text: "View Pokedex", callback_data: "viewPokedex" },
+                  ],
+                  [
+                    { text: "Nickname", callback_data: "nickname" },
+                    { text: "Release", callback_data: "relasePoke" },
+                  ],
+                ],
+              },
+              {
+                chat_id: chatId,
+                message_id,
+              }
+            );
+          });
+      });
+  } catch (err) {
+    console.log(err);
   }
 }
 
-async function userHavePokemon(pokeName , userId) {
+async function isCaughtFailed(wildPokemon, usePokeBall, bot, chatId, message_id , delay , userId , query) {
+  const myPokeList = await makeMyPokeBallList(userId)
+    
+  message = `You're ${ProperMoveName(
+    usePokeBall
+  )} has failed \n\n ${ProperMoveName(wildPokemon().name)} attacking...`;
+  bot.editMessageText(message, {
+    chat_id: chatId,
+    message_id,
+  });
+  query.message.reply_markup.inline_keyboard = myPokeList
+  setTimeout(() => {
+   battleBeginFight(bot, query, "catchPokemon");
+   }, delay);
+}
+
+async function userHavePokemon(pokeName, userId) {
   try {
-    
-    const hasCaught = await userPokeModal.findOne({trainer : userId , name : pokeName})
-    
-    return hasCaught ? true : false
-    
-  } catch(err) {
-    console.log(err)
+    const hasCaught = await userPokeModal.findOne({
+      trainer: userId,
+      name: pokeName,
+    });
+
+    return hasCaught ? true : false;
+  } catch (err) {
+    console.log(err);
   }
 }
 
-function calculateCatchRate(maxHP, currentHP, catchRate, ballModifier, statusModifier) {
-  const numerator = (3 * maxHP - 2 * currentHP) * (catchRate * ballModifier);
-  const denominator = 3 * maxHP;
-  const catchRatePercentage = (numerator / denominator) * statusModifier;
-  return Math.floor(catchRatePercentage);
+function calculateCatchRate(
+  maxHP,
+  currentHP,
+  catchRate,
+  ballRate,
+  statusModifier
+) {
+  const numerator = (1 / maxHP) * 3 + catchRate * ballRate * statusModifier;
+  const denominator =
+    (currentHP * 2 * catchRate * ballRate * statusModifier) / (maxHP * 3);
+  const catchRatePercentage = (numerator - denominator) / 256;
+
+  return Math.floor(catchRatePercentage * 100);
 }
 
 // Example usage
@@ -159,14 +303,23 @@ function calculateCatchRate(maxHP, currentHP, catchRate, ballModifier, statusMod
 // const ballModifier = 1.5; // Let's say we're using a Great Ball
 // const statusModifier = 1; // No status effect
 
-function calculateBallModifier(ballName, level, wildLevel, hasCaught, baseSpeed, types) {
+function calculateBallModifier(
+  ballName,
+  level,
+  wildLevel,
+  hasCaught,
+  baseSpeed,
+  types
+) {
   const ballModifier = ballModifiers[ballName];
 
   if (ballModifier) {
     if (ballName === "level-ball") {
-      let { multiplier } = ballModifier.find(({ threshold }) => level >= wildLevel * threshold) || {};
-        return multiplier = multiplier ? multiplier : 1
-  } else if (ballName === "fast-ball") {
+      let { multiplier } =
+        ballModifier.find(({ threshold }) => level >= wildLevel * threshold) ||
+        {};
+      return (multiplier = multiplier ? multiplier : 1);
+    } else if (ballName === "fast-ball") {
       if (baseSpeed >= ballModifier.speedThreshold) {
         return ballModifier.modifier;
       }
@@ -202,18 +355,19 @@ function calculateBallModifier(ballName, level, wildLevel, hasCaught, baseSpeed,
 
 // const ballModifier = calculateBallModifier(ballName, level, wildLevel, hasCaught, baseSpeed, types);
 
-function pokeBack(bot , query) {
-  const chatId = query.message.chat.id
-  const messageId = query.message.message_id
-  
-  
-  
-  bot.editMessageReplyMarkup({
-    inline_keyboard : reply_markup,
-  } , {
-    chat_id : chatId,
-    message_id : messageId
-  })
+function pokeBack(bot, query) {
+  const chatId = query.message.chat.id;
+  const messageId = query.message.message_id;
+
+  bot.editMessageReplyMarkup(
+    {
+      inline_keyboard: reply_markup,
+    },
+    {
+      chat_id: chatId,
+      message_id: messageId,
+    }
+  );
 }
 
-module.exports = { MyListPokeballs  , pokeBack , pokemonCatch};
+module.exports = { MyListPokeballs, pokeBack, pokemonCatch };
